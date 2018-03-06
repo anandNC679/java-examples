caee42c3095ce6b97a96ca5eb9350f241e007e69
anandzwayam <anand@zwayam.com>
2018-03-06 17:27:32 +0530

    rtyui

--- src/com/aks/collection/hashmap/MapSortingExample.java
+++ src/com/aks/collection/hashmap/MapSortingExample.java
@@ -1,8 +1,5 @@
 package com.aks.collection.hashmap;
 
-
-/*import com.google.common.collect.Maps;
-import com.google.common.collect.Ordering;*/
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -26,41 +23,34 @@ public class MapSortingExample {
         olympicMap.put("China", 2);
         olympicMap.put("Russia", 4);
         olympicMap.put("India", 5);
-        //olympic2012.put("Australia", 4); //adding duplicate value
+        olympicMap.put("Australia", 4); //adding duplicate value
       
-        System.out.println("Unsorted Map in Java : " + olympicMap);
+        System.out.println("Unsorted Map in Java :- " + olympicMap);
       
         //sorting Map e.g. HashMap, Hashtable by keys in Java
         Map<String, Integer> sorted = sortByKeys(olympicMap);
-        System.out.println("Sorted Map in Java by key: " + sorted);
+        System.out.println("Sorted Map in Java by key:- " + sorted);
       
       
         //sorting Map like Hashtable and HashMap by values in Java
         sorted = sortByValues(olympicMap);
-        System.out.println("Sorted Map in Java by values: " + sorted);
+        System.out.println("Sorted Map in Java by values:- " + sorted);
       
       
         //Sorting Map in Java by keys using TreeMap
         Map<String, Integer> sortedMapByKeys = new TreeMap<String,Integer>();
         sortedMapByKeys.putAll(olympicMap);
-        System.out.println("Sorted Map in Java by key using TreeMap : " + sortedMapByKeys);
+        System.out.println("Sorted Map in Java by key using TreeMap :- " + sortedMapByKeys);
  
       
-        //Sorting Map by keys in Java using Google Collections (Guava)
-        //Main benefit is you can specify any ordering like natural or toString or arbitrary
-     /*   Map<String, Integer> sortingUsingGuava = Maps.newTreeMap(Ordering.natural());
-        sortingUsingGuava.putAll(olympic2012);
-        System.out.println("Example to sort Map in Java using Guava : " + sortingUsingGuava);
-      */
-      
-
     }
   
     /*
      * Paramterized method to sort Map e.g. HashMap or Hashtable in Java
      * throw NullPointerException if Map contains null key
      */
-    public static <K extends Comparable,V extends Comparable> Map<K,V> sortByKeys(Map<K,V> map){
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	public static <K extends Comparable,V extends Comparable> Map<K,V> sortByKeys(Map<K,V> map){
         List<K> keys = new LinkedList<K>(map.keySet());
         Collections.sort(keys);
       
@@ -79,12 +69,12 @@ public class MapSortingExample {
      * throw NullPointerException if Map contains null values
      * It also sort values even if they are duplicates
      */
-    public static <K extends Comparable,V extends Comparable> Map<K,V> sortByValues(Map<K,V> map){
+    @SuppressWarnings({"rawtypes","unchecked"})
+	public static <K extends Comparable,V extends Comparable> Map<K,V> sortByValues(Map<K,V> map){
         List<Map.Entry<K,V>> entries = new LinkedList<Map.Entry<K,V>>(map.entrySet());
       
         Collections.sort(entries, new Comparator<Map.Entry<K,V>>() {
-
-            @Override
+        	@Override
             public int compare(Entry<K, V> o1, Entry<K, V> o2) {
                 return o1.getValue().compareTo(o2.getValue());
             }
